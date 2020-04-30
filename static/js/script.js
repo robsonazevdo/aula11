1a221c43088b807998b49a04ffe75a3c8da54fd7
Julio Delgado <juliom6@gmail.com>
2020-04-29 21:49:30 -0300

    depois de adicionar o relogio

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,32 @@
+// fazendo funcionar o relogio
+var r = document.getElementById('relogio');
+
+function atualizaHora(){
+    var hoje = new Date();
+    var h = hoje.getHours();
+    var m = hoje.getMinutes();
+    var s = hoje.getSeconds();
+    if(h < 10)
+        h = '0' + h;
+    if(m < 10)
+        m = '0' + m;
+    if(s < 10)
+        s = '0' + s;
+    r.textContent = h + ":" + m + ":" + s;
+}
+
+setInterval(atualizaHora, 1000); // tempo em milisegundos
+// Crtl + Shift + R
+
+
+
+
+
+
+
+
+//////////////////////////////////
+
 const raiz = document.getElementById('lista');
 
 // var xhr = new XMLHttpRequest();
