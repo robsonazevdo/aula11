b6065ea7949890014dba4c1381707d9763070765
Julio Delgado <juliom6@gmail.com>
2020-04-29 21:01:52 -0300

    adicionando uma template

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/inicio')
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+def inicio():
+    return render_template('index.html')
