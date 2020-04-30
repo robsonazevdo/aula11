62d89a6bd4b87361f45db6129ccd15d2cb54285a
Julio Delgado <juliom6@gmail.com>
2020-04-29 21:23:51 -0300

    commit para producao depois de verificar que tudo deu certo

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
-@app.route('/inicio-alternativo')
+@app.route('/inicio2')
 def outro_inicio():
     return render_template('alternative_index.html')
+
+
+
+## Para rodar o projeto em desenvolvimento
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
